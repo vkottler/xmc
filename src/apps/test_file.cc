95c37a5bf939de23a4351e3a336d06762e0f80f6
Vaughn Kottler <vaughnkottler@gmail.com>
2023-10-16 03:54:38 -0500

    Working on initial blinky program

--- src/apps/test_file.cc
+++ src/apps/test_file.cc
@@ -1,21 +1,53 @@
-/* internal */
-#include "example/sample.h"
+/* third-party */
+#include "hal-xmc4700/generated/structs/port5.h"
 
-int test1(int a, int b)
+void led2_state(bool state)
 {
-    return a + b;
+    if (state)
+    {
+        XMC4700::PORT5->set_OMR_PS8();
+    }
+    else
+    {
+        XMC4700::PORT5->clear_OMR_PS8();
+    }
+}
+
+void led1_state(bool state)
+{
+    if (state)
+    {
+        XMC4700::PORT5->set_OMR_PS9();
+    }
+    else
+    {
+        XMC4700::PORT5->clear_OMR_PS9();
+    }
 }
 
 int main(void)
 {
-    Example::method1();
-    Example::method2();
+    /* Set initial output values. */
+    led1_state(false);
+    led2_state(false);
 
-    float a = 0.0f;
-    for (int i = 0; i < 1000; i++)
+    /* Set drive strength. What class pads are these? */
+    // P5.9
+    // P5.8
+
+    /* Configure pin (recommended before setting to output). */
+
+    /* Set LED pins as outputs. */
+    XMC4700::PORT5->set_IOCR8_PC8(XMC4700::PORT5_IOCR8_PC8::value9);
+    XMC4700::PORT5->set_IOCR8_PC9(XMC4700::PORT5_IOCR8_PC9::value9);
+
+    bool state = false;
+
+    while (true)
     {
-        a *= 2.0f;
-        a /= 2.0f;
+        led1_state(state);
+        led2_state(not state);
+        state = not state;
     }
 
     return 0;
